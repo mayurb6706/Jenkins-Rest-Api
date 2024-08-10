0cebf515a4f08065660285e3197782fa475cf71c
mayurb6706 <mayurbhosale6706@gmail.com>
2024-08-10 22:01:21 +0530

    removed test from ProductServiceApplicationTests class

--- src/test/java/com/cwm/product/ProductServiceApplicationTests.java
+++ src/test/java/com/cwm/product/ProductServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.cwm.product;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
