c9642bf17a7c234e1d7e0070aa2ea0d114f9b493
mayurb6706 <mayurbhosale6706@gmail.com>
2024-08-11 00:41:52 +0530

    Changes for deploying war on tomcat server

--- src/main/java/com/cwm/product/ProductServiceApplication.java
+++ src/main/java/com/cwm/product/ProductServiceApplication.java
@@ -2,9 +2,10 @@ package com.cwm.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class ProductServiceApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
