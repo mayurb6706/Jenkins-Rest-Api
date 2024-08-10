f4cc84a28fc87f2fe074ef8559160e616df5e07d
mayurb6706 <mayurbhosale6706@gmail.com>
2024-08-10 20:25:25 +0530

    removed the swagger response from controller

--- src/main/java/com/cwm/product/controller/ProductController.java
+++ src/main/java/com/cwm/product/controller/ProductController.java
@@ -18,14 +18,11 @@ import com.cwm.product.entity.ProductRequest;
 import com.cwm.product.entity.ProductResponse;
 import com.cwm.product.service.impl.ProductServiceImpl;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(path = "/api/product")
-@Tag(name = "Product Rest Api", description = "Product management APIs")
+@Tag(name = "Product Api", description = "Product management APIs")
 public class ProductController {
 
 	@Autowired
@@ -34,9 +31,6 @@ public class ProductController {
 	// add new product
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
-	@Operation(summary = "Add a new product", description = "Adds a new product to the system")
-	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Product created"),
-			@ApiResponse(responseCode = "400", description = "Invalid input") })
 	public ProductResponse addProduct(@RequestBody ProductRequest product) {
 
 		ProductResponse response = this.productService.addProduct(product);
@@ -45,16 +39,12 @@ public class ProductController {
 	}
 
 	@GetMapping("/{id}")
-	@Operation(summary = "Get a product by ID", description = "Retrieve a product by its ID")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Product found"),
-			@ApiResponse(responseCode = "404", description = "Product not found") })
 	public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
 		ProductResponse response = this.productService.getProductById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping
-	@Operation(summary = "Get all products", description = "Retrieve a list of all products")
 	public ResponseEntity<List<ProductResponse>> getAllProduct() {
 		List<ProductResponse> list = this.productService.getAllProduct();
 		return ResponseEntity.status(HttpStatus.OK).body(list);
@@ -63,10 +53,6 @@ public class ProductController {
 
 	@PutMapping("/{id}")
 	@ResponseStatus(value = HttpStatus.OK)
-	@Operation(summary = "Update a product", description = "Update the details of an existing product")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Product updated"),
-			@ApiResponse(responseCode = "400", description = "Invalid input"),
-			@ApiResponse(responseCode = "404", description = "Product not found") })
 	public ProductResponse updateProduct(@PathVariable(value = "id") Long productId,
 			@RequestBody ProductRequest productRequest) {
 
